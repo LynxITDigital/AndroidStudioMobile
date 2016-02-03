5ecd921e3d55a30ce4c9c2ef690c01ce896066e5
Matt Gale <matthew.gale@lynxit.com.au>
2016-02-03 13:45:09 +1100

    preliminary changes for resizing.

--- app/src/main/java/com/example/blah/mobilestudio/FolderStructureFragment.java
+++ app/src/main/java/com/example/blah/mobilestudio/FolderStructureFragment.java
@@ -1,7 +1,7 @@
 package com.example.blah.mobilestudio;
 
 import android.app.Activity;
-import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.content.Context;
 import android.os.Build;
 import android.os.Bundle;
